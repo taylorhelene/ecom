7040efb702f5e6ead368f8294e9848fd8b498320
taylorhelene <taylorhelene09@gmail.com>
2025-01-15 17:42:40 +0300

    app

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ export default class App extends Component {
     this.routerRef = React.createRef();
   }
 
-  componentDidMount() {
+  async componentDidMount() {
   let user = localStorage.getItem("user");
+  const products = await axios.get('http://localhost:3001/products');
   user = user ? JSON.parse(user) : null;
   this.setState({ user });
   }
