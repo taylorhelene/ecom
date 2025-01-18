47e3b79c217b13620180ad0b3a4a77cb6dfab134
taylorhelene <taylorhelene09@gmail.com>
2025-01-18 17:11:45 +0300

    home

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bulma/css/bulma.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
