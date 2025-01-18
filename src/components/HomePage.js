ba277f188924580b3605109b21c045fe0eef8ea2
taylorhelene <taylorhelene09@gmail.com>
2025-01-18 17:15:01 +0300

    home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,9 +4,12 @@ import withContext from "../withContext";
 const Homepage = props => {
   
   return (
-    <>
-      
-    </>
+    <div className="home container">
+      <div className="hero row">
+        <p className="title">Our Sweet Treats are Handcrafted with Love</p>
+        <img  className="col-sm hero-img" src={process.env.PUBLIC_URL + '/hero.png'}/>
+      </div>
+    </div>
   );
 };
 
