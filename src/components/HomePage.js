002fecf89777c213d6654b6241915dd9a426aab9
taylorhelene <taylorhelene09@gmail.com>
2025-01-20 17:05:04 +0300

    home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,14 @@ const Homepage = props => {
   return (
     <div className="home container">
       <div className="hero row">
-        <p className="title col-sm">Our Sweet Treats are Handcrafted with Love</p>
+        <div className="col-sm">
+          <p className="title ">Our Sweet Treats are Handcrafted with Love</p>
+          <p className="sub-title">Customize Your Cake and Indulge in Flavors. You can get freshly baked 
+            sweer treats that are moist and warm with flavour fillings. Visit our location to try out the flavours
+            first before order
+          </p>
+        </div>
+        
         <img  className="col-sm hero-img" src={process.env.PUBLIC_URL + '/hero.png'}/>
       </div>
     </div>
