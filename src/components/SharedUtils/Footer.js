da0fa4792f630ac4dd0753e92ab0bb2db22ca386
taylorhelene <taylorhelene09@gmail.com>
2025-01-30 11:59:38 +0300

    home

--- src/components/SharedUtils/Footer.js
+++ src/components/SharedUtils/Footer.js
@@ -5,19 +5,19 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Footer = props => {
     return(
-        <div className="container">
+        <div className="container-fluid App">
              <div className="row">
                 <div className="col-sm">
-                    <p className="subtitle">About</p>
+                    <h5 className="subtitle">About</h5>
                 </div>
                 <div className="col-sm">
-                    <p className="subtitle">About</p>
+                    <h5 className="subtitle">About</h5>
                 </div>
                 <div className="col-sm">
-                    <p className="subtitle">About</p>
+                    <h5 className="subtitle">About</h5>
                 </div>
                 <div className="col-sm">
-                    <p className="subtitle">About</p>
+                    <h5 className="subtitle">About</h5>
                 </div>
             </div>
         </div>
