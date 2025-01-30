83fa84a6d3227948b1bd93730ee1088246cafee1
taylorhelene <taylorhelene09@gmail.com>
2025-01-30 11:52:26 +0300

    home

---
+++ src/components/SharedUtils/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import withContext from "../withContext";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+
+const Footer = props => {
+    return(
+        <div className="container">
+             <div className="row">
+                <div className="col-sm">
+                    <p className="subtitle">About</p>
+                </div>
+                <div className="col-sm">
+                    <p className="subtitle">About</p>
+                </div>
+                <div className="col-sm">
+                    <p className="subtitle">About</p>
+                </div>
+                <div className="col-sm">
+                    <p className="subtitle">About</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default withContext(Footer);
