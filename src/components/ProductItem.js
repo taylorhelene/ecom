0617866c06a82c79f55dbacca0a48a804d982203
taylorhelene <taylorhelene09@gmail.com>
2025-03-11 16:31:06 +0300

    products

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,14 +3,15 @@ import React from "react";
 const ProductItem = props => {
   const { product } = props;
   return (
-    <div className=" column">
+    <div className=" p-2 m-2">
       <div className="box">
         <div className="media">
           <div className="media-left">
             <figure className="image is-64x64">
               <img
-                src="https://bulma.io/images/placeholders/128x128.png"
+                src={product.img}
                 alt={product.shortDesc}
+                className="rounded"
               />
             </figure>
           </div>
